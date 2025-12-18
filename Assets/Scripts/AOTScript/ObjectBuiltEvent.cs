87d9411cdad19de417363b6c51e36b54c750f420
17859712212 <1305727157@qq.com>
2025-12-18 12:42:31 +0800

    1

--- Assets/Scripts/AOTScript/ObjectBuiltEvent.cs
+++
@@ -1,9 +0,0 @@
-using GameFramework.ECS.Components; // 需要引用 PlacementType
-
-namespace GameFramework.Events
-{
-    public struct ObjectBuiltEvent
-    {
-        public PlacementType Type;
-    }
-}
